eacd3c94d856e20bcb4b85aa2439fe7acf9e8ff4
Zoe Daniels <zoefaithdaniels094>
2022-11-14 12:33:07 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let ageF = prompt("Enter your age");
+function age(ageF) {
+    if (ageF >= 18) {
+        window.alert("You can continue");
+    } else {
+        window.alert("You do not qualify");
+    }
+}
+age(ageF);
